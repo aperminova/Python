1334429579ef10a6b2004054ce38d01d2807be50
aperminova <aperminova@lohika.com>
2018-09-11 15:21:52 +0300

    phase2

--- json_fix/json_fixtures.py
+++ json_fix/json_fixtures.py
@@ -1,6 +1,6 @@
 class JsonFixtures:
 
-    def create_json_for_issue(self, summary):
+    def create_json_for_issue(self, summary, assignee, priority):
         json = {
             "fields": {
                 "project":
@@ -9,9 +9,22 @@ class JsonFixtures:
                     },
                 "summary": summary,
                 "description": "Creating of an issue using project keys and issue type names using the REST API",
+                "assignee": {"name": assignee},
+                "priority": {"name": priority},
                 "issuetype": {
                     "name": "Bug"
                 }
             }
         }
         return json
+
+    def  create_json_for_search(self, summary):
+        json = {"jql": "summary~" + summary,
+                "startAt": 0,
+                "maxResults": 10
+                }
+
+        return json
+
+
+
