a5cceab1b7f623e03be67b727b244d37e45b1ca9
aperminova <aperminova@lohika.com>
2018-10-02 18:16:11 +0300

    phase4 driver manager

--- ui/driver.py
+++ ui/driver.py
@@ -1,11 +1,14 @@
 
 from selenium import webdriver
+from webdriver_manager.chrome import ChromeDriverManager
 
 def get_driver():
 
     options = webdriver.ChromeOptions()
     options.add_argument("--window-size=1920,1080")
-    driver = webdriver.Chrome('C:\\Users\\aperminova\\PycharmProjects\\Python\\ui\\chromedriver.exe',
-                              chrome_options=options)
+    #driver = webdriver.Chrome('C:\\Users\\aperminova\\PycharmProjects\\Python\\ui\\chromedriver.exe',
+    #                          chrome_options=options)
+
+    driver = webdriver.Chrome(executable_path=ChromeDriverManager().install(), chrome_options=options)
     return driver
 
