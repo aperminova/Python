cdd3beaea96322367e281f34bd5ce1a0c38d0f47
aperminova <aperminova@lohika.com>
2018-10-02 18:54:29 +0300

    phase4

--- ui/driver.py
+++ ui/driver.py
@@ -8,7 +8,5 @@ def get_driver():
     options.add_argument("--window-size=1920,1080")
     #driver = webdriver.Chrome('C:\\Users\\aperminova\\PycharmProjects\\Python\\ui\\chromedriver.exe',
     #                          chrome_options=options)
-
     driver = webdriver.Chrome(executable_path=ChromeDriverManager().install(), chrome_options=options)
     return driver
-
